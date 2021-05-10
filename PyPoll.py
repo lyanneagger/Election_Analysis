040b9f5900d1150aa127738afa0aaf44cb486922
lyanneagger <81982163+lyanneagger@users.noreply.github.com>
2021-05-10 14:27:30 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -26,6 +26,8 @@ total_votes = 0
 
 # Candidate options
 candidate_options = []
+#Declare candidate votes dictionary
+candidate_votes = {}
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
@@ -46,13 +48,25 @@ with open(file_to_load) as election_data:
         if candidate_name not in candidate_options:
             # Add it to the list of candidates
             candidate_options.append(candidate_name)
+            #Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        #Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
 
-    #Print the candidate list
-    print(candidate_options)
+    #Print the candidate vote dictionary
+    print(candidate_votes)
 
-    # To do: perform analysis
-    print(total_votes)
-    
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
 
 #Using the with statement open the file as a text file
 with open(file_to_save,"w") as txt_file:
