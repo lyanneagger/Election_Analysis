cc542b727e9f253a528602af6cb979e09c06ef29
lyanneagger <81982163+lyanneagger@users.noreply.github.com>
2021-05-10 11:01:48 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -12,20 +12,32 @@ now = dt.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
 
+# Add our dependencies.
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources","election_results.csv")
+# Assign a variable to ssave the file to a path
+file_to_save = os.path.join("analysis","election_analysis.txt")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     
+    #Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+    # Print each row in the CSV file.
+    #for row in file_reader:
+        #print (row)
+
     # To do: perform analysis
     print(election_data)
-
-#Create a filename variable to a direct or indirect path to the file
-file_to_save = os.path.join("analysis","election_analysis.txt")
+    
 
 #Using the with statement open the file as a text file
 with open(file_to_save,"w") as txt_file:
