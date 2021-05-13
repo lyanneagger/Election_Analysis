5d919654cfa9540754589208d1a538d2cf10f4d4
lyanneagger <lyanneagger@gmail.com>
2021-05-12 22:55:35 -0400

    update PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # The data we need to retrieve
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
@@ -22,73 +21,61 @@ file_to_load = os.path.join("Resources","election_results.csv")
 # Assign a variable to ssave the file to a path
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+# Initialize variable to count votes
+total_votes = 0
+
+# Candidate options
+candidate_options = []
+#Declare candidate votes dictionary
+candidate_votes = {}
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-    
-    #Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
-
+    
     # Print each row in the CSV file.
     for row in file_reader:
-        print (row)
+        #Add to the total vote count
+        total_votes += 1
 
-    # To do: perform analysis
-    print(election_data)
-    
+        # Print the candidate name from each row
+        candidate_name = row[2]
 
-#Using the with statement open the file as a text file
-with open(file_to_save,"w") as txt_file:
+        # if the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates
+            candidate_options.append(candidate_name)
+            #Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        #Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
 
-    #Write three counties to the file
-=======
-# The data we need to retrieve
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
+    #Print the candidate vote dictionary
+    print(candidate_votes)
 
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
 
-# Add our dependencies.
-import csv
-import os
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
 
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources","election_results.csv")
-# Assign a variable to ssave the file to a path
-file_to_save = os.path.join("analysis","election_analysis.txt")
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-    
-    #Read the file object with the reader function
-    file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
-
-    # Print each row in the CSV file.
-    #for row in file_reader:
-        #print (row)
-
-    # To do: perform analysis
-    print(election_data)
-    
 
 #Using the with statement open the file as a text file
 with open(file_to_save,"w") as txt_file:
 
     #Write three counties to the file
->>>>>>> cc542b727e9f253a528602af6cb979e09c06ef29
     txt_file.write("Counties in the Election\n-----------------------\nArapahoe\nDenver\nJefferson")
