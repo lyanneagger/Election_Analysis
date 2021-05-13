c0b682964c0a6fba41b717a28789e629f992c0b8
lyanneagger <81982163+lyanneagger@users.noreply.github.com>
2021-05-12 23:11:17 -0400

    Added the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -5,13 +5,6 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is ", now)
-
 # Add our dependencies.
 import csv
 import os
@@ -52,10 +45,11 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         #Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
-
-    #Print the candidate vote dictionary
-    print(candidate_votes)
-
+  
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Determine the percentage of votes for each candidate by looping through the counts.
 # 1. Iterate through the candidate list
@@ -64,15 +58,27 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     # 3. Calculate the percentage of votes
     vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
 
-# Winning Candidate and Winning Count Tracker
-winning_candidate = ""
-winning_count = 0
-winning_percentage = 0
+    # 4. Print each candidate's name, vote count, and percentage of votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
+    # Determine the winning vote count and candidate
+    # 1 Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        #2 If true then set winning_count = votes and winning_percentage = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #3 Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
 
+# to do: print out the winning candidate vote count, and percentage to terminal
+winning_candidate_summary = (
+    f"------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage {winning_percentage:.1f}%\n"
+    f"------------------\n")
+print(winning_candidate_summary)
 
 #Using the with statement open the file as a text file
 with open(file_to_save,"w") as txt_file:
